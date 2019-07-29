4583366539a515b6ed983436fc9b6418689814ce
Mick Evans <codemastermick@gmail.com>
2019-07-29 13:08:22 -0400

    feat: add result property to die

--- src/die.ts
+++ src/die.ts
@@ -1,9 +1,12 @@
 export class Die {
   public sides: number;
+  public result: number;
   constructor(sides: number) {
     this.sides = sides;
+    this.result = 0;
   }
   public roll(): number {
-    return Math.floor(Math.random() * this.sides + 1);
+    this.result = Math.floor(Math.random() * this.sides + 1);
+    return this.result;
   }
 }
