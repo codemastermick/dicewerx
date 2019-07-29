2562679a03a100f89491a2ce7bd7d4387c41bd1c
Mick Evans <codemastermick@gmail.com>
2019-07-29 12:45:29 -0400

    feat: added test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+
+const expression="4d6-L*100";
+
+describe('simple validation test', () => {
+  it('should return greater than 0', () => {
+    var result = index.evaluate(expression);
+    expect(result).to.above(0);
+  });
+  it('should return a set of dice', () => {
+    var result = index.forgeDiceFromExpression(expression);
+    const length=Number(index.getDice(expression).split("d")[0]);
+    expect(result.length).to.equal(length);
+  });
+});
