d4d6c98442178dd701b78657d4dd54c0e2997665
Mick Evans <codemastermick@gmail.com>
2019-07-29 12:37:18 -0400

    feat: create export module

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { dropHighest, dropLowest, sum } from "./operations";
+export { getDice, getDrop, getOperator, evaluate } from "./parser";
+export { Die } from "./die";
+export { forgeDiceFromExpression } from "./diceforge";
