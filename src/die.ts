3db786b5da0a8ef9c0fb883aca3866897d7f6b12
Mick Evans <codemastermick@gmail.com>
2019-07-30 09:44:31 -0400

    chore: code cleanup

--- src/die.ts
+++ src/die.ts
@@ -1,12 +1,9 @@
 export class Die {
   public sides: number;
-  public result: number;
   constructor(sides: number) {
     this.sides = sides;
-    this.result = 0;
   }
   public roll(): number {
-    this.result = Math.floor(Math.random() * this.sides + 1);
-    return this.result;
+    return Math.floor(Math.random() * this.sides + 1);
   }
 }
