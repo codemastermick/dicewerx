6bcabbe8a3b6ab5f33a46fb126eebcfb1f2521db
Mick Evans <codemastermick@gmail.com>
2019-08-02 08:46:25 -0400

    chore: fix tests and remove unused test

--- test/test.js
+++ test/test.js
@@ -5,22 +5,17 @@ var index = require('../dist/index.js');
 const expression="4d6-L*100";
 
 describe('simple validation test', () => {
-  it('should return greater than 0', () => {
+  it('should return a number within range', () => {
     var result = index.evaluate(expression);
-    expect(result).to.be.above(0);
-  });
-  it('should return a set of dice', () => {
-    var result = index.forgeDiceFromExpression(expression);
-    const length=Number(index.getDice(expression).split("d")[0]);
-    expect(result.length).to.equal(length);
+    expect(result).to.be.above(299).and.to.be.below(1801);
   });
   it('should return 2', () => {
-    var dice=index.forgeDiceFromExpression("2d6");
+    var dice=index.forge("2d6");
     var result = index.rollMin(dice);
     expect(result).to.be.equal(2);
   });
   it('should return 12', () => {
-    var dice=index.forgeDiceFromExpression("2d6");
+    var dice=index.forge("2d6");
     var result = index.rollMax(dice);
     expect(result).to.be.equal(12);
   });
