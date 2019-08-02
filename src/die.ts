a000bbca8f97ca9524b70c11c3a342b81170387e
Mick Evans <codemastermick@gmail.com>
2019-08-02 03:42:36 -0400

    feat: add result and autoroll to new dice

--- src/die.ts
+++ src/die.ts
@@ -1,7 +1,9 @@
 export class Die {
   public sides: number;
+  public result: number;
   constructor(sides: number) {
     this.sides = sides;
+    this.result = this.roll();
   }
   public roll(): number {
     return Math.floor(Math.random() * this.sides + 1);
