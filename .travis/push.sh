886273d897fed19b4e0d7c7d4a89a09d7e7845bd
Mick Evans <codemastermick@gmail.com>
2019-08-05 07:32:24 -0400

    chore: try something new

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 setup_git() {
-    git config --global user.name "Travis CI"
+    git config --global user.name "$GH_TOKEN"
     git config --global user.email "travis@travis-ci.org"
 }
 
