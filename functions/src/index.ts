77bf87e72b98edeca1fc730fe225c537f7c46e48
Mick Evans <codemastermick@gmail.com>
2019-08-05 10:58:00 -0400

    feat: create dicewerx api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,42 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import { evaluate, rollMax, rollMin, forge } from "dicewerx";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// BEGIN DICEWERX API
+export const roll = functions.https.onRequest((request, response) => {
+    const expression = request.query.exp;
+    response.send(evaluate(expression));
+});
+
+export const max = functions.https.onRequest((request, response) => {
+    const expression = request.query.exp;
+    response.send(rollMax(expression));
+});
+
+export const min = functions.https.onRequest((request, response) => {
+    const expression = request.query.exp;
+    response.send(rollMin(expression));
+});
+
+export const getDice = functions.https.onRequest((request, response) => {
+    const expression = request.query.exp;
+    response.send(forge(expression));
+});
+
+export const roll20 = functions.https.onRequest((request, response) => {
+    const result = evaluate("1d20");
+    response.send(result);
+});
+
+export const rollStats = functions.https.onRequest((request, response) => {
+    const results = [6];
+    for (let i = 0; i < 6; i++) {
+        results[i] = evaluate("4d6-L");
+    }
+    response.send(results);
+});
+
+export const rollPercentile = functions.https.onRequest((request, response) => {
+    const result = evaluate("1d100");
+    response.send(result);
+});
+// END DICEWERX API
