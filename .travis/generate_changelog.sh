b89700043f0b46dfd90689623c818f32d10bfa78
Mick Evans <codemastermick@gmail.com>
2019-08-05 09:20:08 -0400

    refactor: rename upload_changelog

---
+++ .travis/generate_changelog.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+# Stage the modified files in docs
+git add -f CHANGELOG.md
+# Create a new commit with a custom build message
+# with "[skip ci]" to avoid a build loop
+# and Travis build number for reference
+git commit -m "Regenerate changelog: build $TRAVIS_BUILD_NUMBER [skip ci]"
+git checkout master
+npm run version
+commit_changelog
