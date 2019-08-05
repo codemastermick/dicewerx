30b0d814dc5db36fcd51cd52557004f9d76b2ac5
Mick Evans <codemastermick@gmail.com>
2019-08-05 06:18:37 -0400

    feat: publish docs on success

---
+++ .travis/push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_git() {
+    git config --global user.name "Travis CI"
+    git config --global user.email "travis@travis-ci.org"
+}
+
+commit_website_files() {
+    git add ./docs/
+    git commit --message "chore: regenerate docs build:$TRAVIS_BUILD_NUMBER [ci skip]"
+}
+
+upload_files() {
+    git remote add origin https://${GH_TOKEN}@github.com/codemastermick/dicewerx.git >/dev/null 2>&1
+    git push --quiet --set-upstream origin master
+}
+
+setup_git
+commit_website_files
+upload_files
