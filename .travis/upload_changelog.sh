f63e09547ab3650415ae2689283190b6e4e01f68
Mick Evans <codemastermick@gmail.com>
2019-08-05 09:16:56 -0400

    chore: reduce git commits made by travis

--- .travis/upload_changelog.sh
+++ .travis/upload_changelog.sh
@@ -10,22 +10,6 @@ commit_changelog() {
     git commit -m "Regenerate changelog: build $TRAVIS_BUILD_NUMBER [skip ci]"
 }
 
-upload_files() {
-    # Remove existing "origin"
-    git remote rm origin
-    # Add new "origin" with access token in the git URL for authentication
-    git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/dicewerx.git >/dev/null 2>&1
-    git push origin master --quiet
-}
-
 git checkout master
 npm run version
 commit_changelog
-
-# Attempt to commit to git only if "git commit" succeeded
-if [ $? -eq 0 ]; then
-    echo "A new commit exists for CHANGELOG. Uploading to GitHub"
-    upload_files
-else
-    echo "No changes in changelog file. Nothing to do"
-fi
--- .travis/upload_docs.sh
+++ .travis/upload_docs.sh
@@ -10,21 +10,5 @@ commit_docs() {
     git commit -m "Regnerate docs: build $TRAVIS_BUILD_NUMBER [skip ci]"
 }
 
-upload_files() {
-    # Remove existing "origin"
-    git remote rm origin
-    # Add new "origin" with access token in the git URL for authentication
-    git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/dicewerx.git >/dev/null 2>&1
-    git push origin master --quiet
-}
-
 git checkout master
 commit_docs
-
-# Attempt to commit to git only if "git commit" succeeded
-if [ $? -eq 0 ]; then
-    echo "A new commit with changed documentation exists. Uploading to GitHub"
-    upload_files
-else
-    echo "No changes in documentation files. Nothing to do"
-fi
