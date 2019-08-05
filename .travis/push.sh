0dd338e1514b357817bed25ec38b3718273aff78
Mick Evans <codemastermick@gmail.com>
2019-08-05 07:15:09 -0400

    chore: pretty sure I need this line

--- .travis/push.sh
+++ .travis/push.sh
@@ -11,6 +11,7 @@ commit_files() {
 }
 
 upload_files() {
+    git remote rm origin
     git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/dicewerx.git >/dev/null 2>&1
     git push origin master --quiet
 }
