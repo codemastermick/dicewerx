e2f65d1e4a92ed5db6913e6c6b43f9379e813527
Mick Evans <codemastermick@gmail.com>
2019-08-05 11:03:41 -0400

    refactor: change import to dicebag.*

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,42 +1,42 @@
 import * as functions from "firebase-functions";
-import { evaluate, rollMax, rollMin, forge } from "dicewerx";
+import * as dicebag from "dicewerx";
 
 // BEGIN DICEWERX API
 export const roll = functions.https.onRequest((request, response) => {
     const expression = request.query.exp;
-    response.send(evaluate(expression));
+    response.send(dicebag.evaluate(expression));
 });
 
 export const max = functions.https.onRequest((request, response) => {
     const expression = request.query.exp;
-    response.send(rollMax(expression));
+    response.send(dicebag.rollMax(expression));
 });
 
 export const min = functions.https.onRequest((request, response) => {
     const expression = request.query.exp;
-    response.send(rollMin(expression));
+    response.send(dicebag.rollMin(expression));
 });
 
 export const getDice = functions.https.onRequest((request, response) => {
     const expression = request.query.exp;
-    response.send(forge(expression));
+    response.send(dicebag.forge(expression));
 });
 
 export const roll20 = functions.https.onRequest((request, response) => {
-    const result = evaluate("1d20");
+    const result = dicebag.evaluate("1d20");
     response.send(result);
 });
 
 export const rollStats = functions.https.onRequest((request, response) => {
     const results = [6];
     for (let i = 0; i < 6; i++) {
-        results[i] = evaluate("4d6-L");
+        results[i] = dicebag.evaluate("4d6-L");
     }
     response.send(results);
 });
 
 export const rollPercentile = functions.https.onRequest((request, response) => {
-    const result = evaluate("1d100");
+    const result = dicebag.evaluate("1d100");
     response.send(result);
 });
 // END DICEWERX API
