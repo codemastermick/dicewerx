b32a7a9d94c65c28818c3cd80902056bc7bb2426
Mick Evans <codemastermick@gmail.com>
2019-08-05 07:48:08 -0400

    chore: try set-url instead

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,10 +12,8 @@ git commit -m "chore: regenerate docs build:$TRAVIS_BUILD_NUMBER [ci skip]"
 # Attempt to commit to git only if "git commit" succeeded
 if [ $? -eq 0 ]; then
     echo "A new commit with changed documentation exists. Uploading to GitHub"
-    # Remove existing "origin"
-    git remote rm origin
     # Add new "origin" with access token in the git URL for authentication
-    git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/dicewerx.git >/dev/null 2>&1
+    git remote set-url origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/dicewerx.git >/dev/null 2>&1
     git push origin master --quiet
 else
     echo "No changes in documentation files. Nothing to do"
